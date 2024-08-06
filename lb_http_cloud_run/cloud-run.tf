89fd0f12b95dd0ed460ca9b0caf5600888db7683
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-06 13:14:29 +0100

    Update cloud-run.tf

--- lb_http_cloud_run/cloud-run.tf
+++ lb_http_cloud_run/cloud-run.tf
@@ -7,3 +7,32 @@ module "cloud_run" {
   location               = var.region
   image                  = "gcr.io/cloudrun/hello"
 }
+
+resource "google_cloud_run_service_iam_member" "public-access" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+#resource "google_cloud_run_service" "default" {
+  #name     = "example"
+  #location = var.region
+  #project  = var.project_id
+
+  #template {
+    #spec {
+      #containers {
+        #image = "gcr.io/cloudrun/hello"
+      #}
+    #}
+  #}
+  #metadata {
+    #annotations = {
+      # For valid annotation values and descriptions, see
+      # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
+      #"run.googleapis.com/ingress" = "all"
+    #}
+  #}
+#}
