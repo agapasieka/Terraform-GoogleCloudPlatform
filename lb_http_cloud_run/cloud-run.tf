520046b960a0b141f9d4f3e8f18d26d889d2652e
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-06 13:30:01 +0100

    Update cloud-run.tf

--- lb_http_cloud_run/cloud-run.tf
+++ lb_http_cloud_run/cloud-run.tf
@@ -1,38 +1,15 @@
+## Private Cloud Run Service allowing unauthenticated invocations, accesible via HTTP Load Balancer
 module "cloud_run" {
   source  = "GoogleCloudPlatform/cloud-run/google"
   version = "~> 0.12.0"
 
-  service_name           = "cloud-run-example"
-  project_id             = var.project_id
-  location               = var.region
-  image                  = "gcr.io/cloudrun/hello"
+  service_name = "cloudrun-example-tf"
+  project_id   = var.project_id
+  location     = var.region
+  image        = "gcr.io/cloudrun/hello"
+  members      = ["allUsers"]
+  service_annotations = {
+    "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing",
+    "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
+  }
 }
-
-resource "google_cloud_run_service_iam_member" "public-access" {
-  location = google_cloud_run_service.default.location
-  project  = google_cloud_run_service.default.project
-  service  = google_cloud_run_service.default.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-#resource "google_cloud_run_service" "default" {
-  #name     = "example"
-  #location = var.region
-  #project  = var.project_id
-
-  #template {
-    #spec {
-      #containers {
-        #image = "gcr.io/cloudrun/hello"
-      #}
-    #}
-  #}
-  #metadata {
-    #annotations = {
-      # For valid annotation values and descriptions, see
-      # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
-      #"run.googleapis.com/ingress" = "all"
-    #}
-  #}
-#}
